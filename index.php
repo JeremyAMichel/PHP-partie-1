46d04081364aedb081e4f6caaf06fefadb2f4c69
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-06-27 20:00:04 +0200

    correction

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+
+// exercice 1 //
+// $name = 'Jérémy';
+
+// echo $name;
+
+// exercice 2 //
+// $lastname = 'Michel';
+// $firstname = 'Jérémy';
+// $age = 27;
+
+// echo $firstname . ' ' . $lastname . ' ' . $age . ' ans.'
+
+// exercice 3 //
+// $km = 1;
+// echo $km;
+// $km = 3;
+// echo $km;
+// $km = 125;
+// echo $km;
+
+// exercice 4 //
+// $int = 5;
+// $string = 'une chaine de caractères';
+// $boolean = true;
+// $float = 11.4;
+// echo 'int:'.$int.' string:'.$string.' boolean:'.$boolean.' float:'.$float
+
+// exercice 5 //
+// $int;
+// var_dump($int);
+// $int = 5;
+// var_dump($int);
+
+// exercice 6 //
+// $name = 'Jérémy';
+// echo 'Bonjour '.$name.', comment vas-tu ?';
+
+// exercice 7 //
+// $lastname = 'Michel';
+// $firstname = 'Jérémy';
+// $age = 27;
+// echo "Bonjour " . $lastname .' '. $firstname . ", tu as " . $age . " ans";
+
+// exercice 8 //
+$premierResultat = 3+4;
+$deuxiemeResultat = 5*20;
+$troisiemeResultat = 45/5;
+
+echo $premierResultat . ' ' . $deuxiemeResultat . ' ' . $troisiemeResultat;
+
+
+?>
